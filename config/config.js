7a0809d56e12456264d55460e857afe023b2a194
prabhjot <prabhjotsingh@usatechnologyllc.com>
2019-02-19 16:48:31 +0530

    changes live mongoose server

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-// const config = {url:'mongodb+srv://shopuser:shopuser@cluster0-6so3n.mongodb.net/shop?retryWrites=true'};
+const config = {url:'mongodb+srv://shopuser:shopuser@cluster0-6so3n.mongodb.net/shop?retryWrites=true'};
 
-const config = {url:'mongodb://localhost/shopping'};
+//const config = {url:'mongodb://localhost/shopping'};
 module.exports = config;
