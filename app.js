1983e081206adf8ba864ba5dde425e4c0c21f65d
Manish Sharma <manish198646@yahoo.com>
2019-02-20 13:41:31 +0530

    cors implementation

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ let isProduction = process.env.NODE_ENV === 'production';
 // initialize our express app
 const app = express();
 
+app.use(cors());
+
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
